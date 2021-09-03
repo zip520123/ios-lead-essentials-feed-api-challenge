b09b71e1fb8e08ed80c8200b53ed103651c2eaff
zip520123 <zip520123@yahoo.com.tw>
2021-09-03 16:26:40 +0100

    Delivers success with no Items on 200 HTTP Response with empty JSONList

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,37 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url, completion: { result in
 			switch result {
-			case .success((_, _)):
-				completion(.failure(RemoteFeedLoader.Error.invalidData))
+			case let .success((data, response)):
+				if response.statusCode == 200, let root = try? JSONDecoder().decode(RemoteFeedLoadImagesRoot.self, from: data) {
+					completion(.success(root.items.map { $0.feedImage }))
+				} else {
+					completion(.failure(RemoteFeedLoader.Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
 			}
 		})
+	}
+}
+
+private struct RemoteFeedLoadImagesRoot: Decodable {
+	let items: [RemoteFeedLoadImage]
+}
+
+private struct RemoteFeedLoadImage: Decodable {
+	let id: UUID
+	let desc: String?
+	let location: String?
+	let url: URL
+
+	enum Codingkeys: String, CodingKey {
+		case id = "image_id"
+		case desc = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
+	}
+
+	var feedImage: FeedImage {
+		FeedImage(id: id, description: desc, location: location, url: url)
 	}
 }
